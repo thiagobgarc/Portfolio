453106ba5b64a33df2cc3e453b0b33a966bbf020
thiagobgarc <thiagobuenogarcia1@gmail.com>
2023-07-19 16:25:28 -0400

    Adding css to about.css

--- src/components/About.js
+++ src/components/About.js
@@ -10,13 +10,15 @@ export const About = () => {
             <div>
         <h1 className="title">About Me</h1>
         <div>
-        <h3 >
+        <h3 className=".noHiddenText">
         Hello and welcome to my software engineering portfolio! My name is Thiago Garcia and I am a software engineer with a passion for creating elegant and effective solutions to complex problems.
+        
 <br/>
 <br/>
 My journey into software engineering began with a desire to learn how technology could be used to solve real-world problems. I decided to pursue a career in software engineering. I enrolled in General Assembly's immersive bootcamp, where I gained hands-on experience with various programming languages and frameworks, including Javascript, React.js, Node.js, and Python.
-<br/>
-<br/>
+</h3>
+
+<h3 className="hiddenTextMobile">
 During the bootcamp, I worked on a number of projects that allowed me to apply my new skills and knowledge to real-world problems. I collaborated with other students to develop web applications, mobile applications, and full-stack applications that integrated with various databases and APIs.
 <br/>
 <br/>
